9397fb66b4b62742d622d9d659cd5d256a2cf3ba
leonardo <leonardocamelo20@gmail.com>
2024-12-18 16:25:04 -0300

    feat: redecoration header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,40 +1,54 @@
 import * as React from "react";
-import { Container } from "@mui/material";
 import styled from "styled-components";
 
 export const Header = () => {
   return (
     <AppBar>
-      <div style={{ display: "flex", marginBottom: 40 }}>
+      <div style={{ display: "flex" }}>
         <Title>Code</Title>
-        <Title $color>
-          Learn
-        </Title>
+        <Title $color>Learner</Title>
       </div>
-      <Container
-        sx={{
-          maxWidth: {
-            xs: "xs",
-            sm: "sm",
-            md: "md",
-            lg: "md",
-          },
-        }}
-      >
+      <Input>
         <input placeholder="pesquisar no blog" id="input" />
-      </Container>
+        <Divider />
+        <div style={{ display: "flex", gap: 10 }}>
+          <a href="#">
+            <HeaderButton>sign in</HeaderButton>
+          </a>
+          <a href="#">
+            <HeaderButton>sign in</HeaderButton>
+          </a>
+        </div>
+      </Input>
     </AppBar>
   );
 };
 
 const AppBar = styled.div`
-  padding: 40px 0px;
   display: flex;
-  flex-direction: column;
+  padding: 20px 40px;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   background: #202024;
 `;
+
+const Input = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  width: 600px;
+`;
+
+const Divider = styled.div`
+  height: 30px;
+  width: 1px;
+  background: #4b4950;
+`;
+
+const HeaderButton = styled.button`
+  width: 60px;
+  height: 30px;
+`
 
 const Title = styled.h1<{ $color?: boolean }>`
   color: ${(props) => (props.$color ? "#FFFFFF" : "#677CE0")};
