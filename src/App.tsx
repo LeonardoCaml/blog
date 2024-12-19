b54f2336305ac28fbdf374b0e7a279f5dcbe9a77
leonardo <leonardocamelo20@gmail.com>
2024-12-19 18:47:47 -0300

    :sparkless: feat: add sidebars

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from "./components/header";
 import { Post } from "./components/post";
 import styled from "styled-components";
 import "./style/home.css";
+import { PostInToplist } from "./components/postInToplist";
 
 const postCard = [
   {
@@ -11,6 +12,8 @@ const postCard = [
     title: "O que é linguagem de programação? Conheça as principais",
     description:
       "Uma das mais populares vertentes da tecnologia da informação, a área de programação segue tendo muita demanda de trabalho justamente pela velocidade com que dispositivos tecnológicos vêm avançando.",
+    category: "Conhecimento",
+    specify: "Frontend / Backend",
   },
   {
     id: 2,
@@ -18,6 +21,8 @@ const postCard = [
     title: "GitHub agora permite fazer login sem precisar de senha",
     description:
       "O GitHub anunciou nesta quarta-feira (12) o acesso a partir das passkeys, método de autenticação sem senhas. A novidade está disponível em uma versão beta pública e pode substituir a autenticação em dois fatores.",
+    category: "Curiosidades",
+    specify: "Frontend / Backend",
   },
   {
     id: 3,
@@ -25,6 +30,8 @@ const postCard = [
     title: "Por que os hiperlinks são azuis em sua maioria??",
     description:
       "Quem navega na internet, certamente já percebeu que ela conta com diversos recursos para tornar a nossa vida mais fácil. Entre essas opções podemos mencionar os hiperlinks – uma palavra ou termo clicável que direciona o leitor.",
+    category: "Curiosidades",
+    specify: "Frontend",
   },
   {
     id: 4,
@@ -32,6 +39,8 @@ const postCard = [
     title: "Primeiros passos com HTML e CSS",
     description:
       "Aprender HTML e CSS é o ponto de partida para qualquer iniciante em programação web. Entenda como criar páginas básicas e aplicar estilos para deixar sua aplicação mais atraente.",
+    category: "Conhecimento",
+    specify: "Frontend",
   },
   {
     id: 5,
@@ -39,6 +48,8 @@ const postCard = [
     title: "Os erros mais comuns ao aprender a programar",
     description:
       "Iniciar na programação pode ser desafiador. Conheça os erros mais frequentes dos iniciantes e aprenda como evitá-los para progredir mais rápido nos seus estudos.",
+    category: "Conhecimentos",
+    specify: "Frontend / Backend",
   },
 ];
 
@@ -46,24 +57,94 @@ const App = () => {
   return (
     <Wrapper id="home">
       <Header />
-      <Wrapper>
-        {postCard.map((item) => (
-          <Post
-            key={item.id}
-            data={item.data}
-            title={item.title}
-            description={item.description}
-          />
-        ))}
-      </Wrapper>
+      <Body>
+        <SidebarLeft>
+          <h1>Categorias</h1>
+          <p>Curiosidades</p>
+          <p>Novidades</p>
+          <p>Conhecimento</p>
+          <h1>Específicos</h1>
+          <p>Frontend</p>
+          <p>Backend</p>
+          <p>Infraestrutura</p>
+          <p>DevOps</p>
+          <p>UX/UI</p>
+          <p>QA</p>
+        </SidebarLeft>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 30,
+            margin: 20,
+          }}
+        >
+          {postCard.map((item) => (
+            <Post
+              key={item.id}
+              data={item.data}
+              title={item.title}
+              description={item.description}
+            />
+          ))}
+        </div>
+        <SidebarRight>
+          <h1>mais procurados</h1>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 10,
+            }}
+          >
+            {postCard.map((item) => (
+              <PostInToplist
+                key={item.id}
+                title={item.title}
+                data={item.data}
+                category={item.category}
+                specify={item.specify}
+              />
+            ))}
+          </div>
+        </SidebarRight>
+      </Body>
     </Wrapper>
   );
-}
+};
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+`;
+
+const Body = styled.div`
+  display: grid;
+  grid-template-columns: 0.3fr 1fr 0.3fr;
   gap: 30px;
 `;
 
-export default App
+const SidebarLeft = styled.div`
+  border-right: 1px solid #4b4950;
+  background: #17171a;
+  color: #ffffff;
+  height: 100vh;
+  padding: 40px 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const SidebarRight = styled(SidebarLeft)`
+  align-items: center;
+  border-left: 1px solid #252529;
+  min-height: fit-content;
+`;
+
+const Divider = styled.div`
+  width: "100%";
+  height: 2px;
+  background: #4b4950;
+`;
+
+export default App;
