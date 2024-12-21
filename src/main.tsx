9c0a9f7bb8fe69ab9c7ac78a62edc7f8b816ed95
leonardo <leonardocamelo20@gmail.com>
2024-12-21 13:33:39 -0300

    :sparkles:	feat:  Add a React Router and create many routers

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import Home from "./App";
 import { BrowserRouter, Route, Routes } from "react-router";
+import SignUp from "./pages/SignIn";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index element={<App />} />
+        <Route index element={<Home />} />
+        <Route path="signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
